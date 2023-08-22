0eae4c39f7766c0ea41522783484ba7f9a820287
krishpathak <krishpathak20@gmail.com>
2023-08-22 22:55:17 +0530

    updated code

--- calculator.cpp
+++ calculator.cpp
@@ -1,116 +1,156 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
 
-int main() 
+using namespace std;
+int factorial(int n)
 {
-    
-    do 
+    if(n==0||n==1)
     {
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+double sin(double x, double terms) {
+    double result = 0.0;
+    for (int n = 0; n < terms; ++n) {
+        double term = (pow(-1, n) * pow(x, 2 * n + 1)) / factorial(2 * n + 1);
+        result += term; // Accumulate the terms
+    }
+    return result;
+}
+
+double cos(double x, double terms) {
+    double result = 0.0;
+    for (int p = 0; p < terms; ++p) {
+        double term = (pow(-1, p) * pow(x, 2 * p)) / factorial(2 * p);
+        result += term; // Accumulate the terms
+    }
+    return result;
+}
+double Exponential(double x, int terms) {
+    double result = 1.0;
+    for (int n = 1; n < terms; ++n) {
+        double term = pow(x, n) / factorial(n);
+        result += term;
+    }
+    return result;
+}
+int main() {
+    int choice;
+    do {
         cout << "Which operation do you want to perform? (Enter the corresponding number)" << endl;
         cout << "1. Addition" << endl << "2. Subtraction" << endl << "3. Multiplication" << endl;
         cout << "4. Division" << endl << "5. Remainder" << endl << "6. Square root" << endl;
-        cout << "7. Square" << endl << "8. Trigonometric function" << endl << "9. Exponential function" << endl;
-        cout << "10. Absolute value" << endl << "11. Power" << endl;
-        
-        
-        int op;
-        cin>>op;
-        double n1,n2;
-        int n3;
-        switch(op)
-        {
-            case 1:
-            cout<<"Enter two numbers"<<endl;
-            cin>>n1>>n2;
-            cout<<"The sum of "<<n1<<" and "<<n2<<" is  "<<n1+n2<<endl;
-            break;
-            case 2:
-            cout<<"Enter two numbers"<<endl;
-            cin>>n1>>n2;
-            cout<<"The substraction of "<<n1<<" and "<<n2<<" is  "<<n1-n2<<endl;
-            break;
-            case 3:
-            cout<<"Enter two numbers"<<endl;
-            cin>>n1>>n2;
-            cout<<"The multiplication of "<<n1<<" and "<<n2<<" is  "<<n1*n2<<endl;
-             break;
-             case 4:
-             cout<<"Enter two numbers"<<endl;
-            cin>>n1>>n2;
-            cout<<"The divison of "<<n1<<" and "<<n2<<"is  "<<n1/n2<<endl;
-            break;
-            case 5:
-            int n4,n5;
-            cout<<"Enter two numbers"<<endl;
-            cin>>n4>>n5;
-            cout<<"The remainder when "<<n4<<" is divided by "<<n5<< " is "<<n4%n5<<endl;
-            break;
-            case 8:
-             {
-             char trig;
-             double a;
-             cout<<"Enter trignometric function(Enter first letter)"<<endl;
-             cout<<"a.sin"<<endl<<"b.cos"<<endl<<"c.tan"<<endl<<"d.cosec"<<endl<<"e.sec"<<endl<<"f.cot"<<endl;
-             cin>>trig;
-             
-             cout<<"Enter the value in degree"<<endl;
-             cin>>a;
-             a=(M_PI*a)/180.0;
-            switch(trig)
-            {
-                case 'a':
-                cout<<"The sin value is"<<sin(a)<<endl;
-                break;
-                case 'b':
-                cout<<"The cos value is"<<cos(a)<<endl;
-                break;
-                case 'c':
-                cout<<"The tan value is"<<tan(a)<<endl;
-                break;
-                case 'd':
-                cout<<"The cosec value is"<<(1/sin(a))<<endl;
-                break;
-                case 'e':
-                cout<<"The sec value is"<<(1/cos(a))<<endl;
-                break;
-                case 'f':
-                cout<<"The cot value is"<<(1/tan(a))<<endl;
-                break;
-                default:
-                cout<<"Something went wrong:("<<endl;
-                break;
-            } break;
-            case 6:
-            cout<<"Enter the value"<<endl;
-            cin>>n3;
-            cout<<"The square root of "<<n3<<" is "<<sqrt(n3)<<endl;
-            break;
-            case 7:
-            cout<<"Enter the value"<<endl;
-            cin>>n3;
-            cout<<"The square of "<<n3<<" is "<<n3*n3<<endl;
-            break;
-            case 9:
-            cout<<"Enter the value"<<endl;
-            cin>>n3;
-            cout<<"The exponential of "<<n3<<" is "<<exp(n3)<<endl;
-            break;
-            case 10:
-            cout<<"Enter the value"<<endl;
-            cin>>n3;
-            cout<<"The absolute value of "<<n3<<" is "<<abs(n3)<<endl;
-            break;
-            case 11:
-            cout<<"Enter two numbers"<<endl;
-            cin>>n1>>n2;
-            cout<<"The value of "<<n1<<" raised "<<n2<<" is "<<pow(n1,n2)<<endl;
-            default:
-            cout<<"Something went wrong:(";
+        cout << "7. Square" << endl << "8. Trigonometric function" << endl << "9. Exponential function" << endl << "10. Exit" << endl;
+        cin >> choice;
+
+        if (choice == 10) {
+            cout << "Exiting the program." << endl;
             break;
         }
-    }
-}
-    while(1);
+
+        double n1, n2, result;
+        int n3;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter two numbers: ";
+                cin >> n1 >> n2;
+                result = n1 + n2;
+                cout << "The sum is: " << result << endl;
+                break;
+            case 2:
+                cout << "Enter two numbers: ";
+                cin >> n1 >> n2;
+                result = n1 - n2;
+                cout << "The subtraction result is: " << result << endl;
+                break;
+            case 3:
+                cout << "Enter two numbers: ";
+                cin >> n1 >> n2;
+                result = n1 * n2;
+                cout << "The multiplication result is: " << result << endl;
+                break;
+            case 4:
+                cout << "Enter two numbers: ";
+                cin >> n1 >> n2;
+                if (n2 != 0) {
+                    result = n1 / n2;
+                    cout << "The division result is: " << result << endl;
+                } else {
+                    cout << "Cannot divide by zero." << endl;
+                }
+                break;
+            case 5:
+                cout << "Enter two numbers: ";
+                cin >> n1 >> n2;
+                if (n2 != 0) {
+                    result = fmod(n1, n2);
+                    cout << "The remainder is: " << result << endl;
+                } else {
+                    cout << "Cannot divide by zero." << endl;
+                }
+                break;
+            case 6:
+                cout << "Enter a number: ";
+                cin >> n3;
+                if (n3 >= 0) {
+                    result = sqrt(n3);
+                    cout << "The square root is: " << result << endl;
+                } else {
+                    cout << "Cannot compute square root of a negative number." << endl;
+                }
+                break;
+            case 7:
+                cout << "Enter a number: ";
+                cin >> n3;
+                result = n3 * n3;
+                cout << "The square is: " << result << endl;
+                break;
+            case 8:
+                char trigChoice;
+                double x;
+                cout << "Enter the trigonometric function (sin(s),cos(c), tan(t), cot(d), cosec(e),sec(f) ";
+                cin >> trigChoice;
+                cout << "Enter the angle in degrees: ";
+                cin >> x;// Convert to radians
+                x = x * M_PI / 180.0; 
+                switch (trigChoice) {
+                    case 's':
+                      
+                      cout<<"the value of sin is"<<sin(x,10)<<endl;
+                        break;
+                    case 'c':
+                      cout<<"the value of cos is"<<cos(x,10)<<endl;
+                       break;
+                    case 't':
+                      cout<<"the value of tan is"<<sin(x,10)/cos(x,10)<<endl;
+                       break;
+                    case 'd':
+                      cout<<"the value of cot is"<<cos(x,10)/sin(x,10)<<endl;
+                        break;
+                    case 'e':
+                        cout<<"the value of cosec is"<<1/sin(x,10)<<endl;
+                        break;
+                    case 'f':
+                        cout<<"the value of sec is"<<1/cos(x,10)<<endl;
+                        break;
+                    default:
+                        cout << "Invalid trigonometric function choice." << endl;
+                        break;
+                }
+                break;
+            case 9:
+                cout << "Enter a number: ";
+                cin >> n3;
+                cout << "The exponential value is: " << Exponential(n3,10) << endl;
+                break;
+            default:
+                cout << "Invalid choice." << endl;
+                break;
+        }
+
+    } while (true);
+
     return 0;
 }
